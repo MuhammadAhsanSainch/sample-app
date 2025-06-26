0087a8f2efba905790f174656daad5f39e04254e
muhammadahsan <muhammad.ahsan@futurbyte.ae>
2025-06-26 12:37:16 +0500

    cosmetics: cosmetics applied

--- lib/utilities/app_initializer.dart
+++ lib/utilities/app_initializer.dart
@@ -1,11 +1,10 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:path_to_water/api_core/network_controller.dart';
-
+import 'app_exports.dart';
 import '../firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import '../notification_services/fcm_controller.dart';
 import '../notification_services/notification_service.dart';
-import 'app_exports.dart';
+import 'package:path_to_water/api_core/network_controller.dart';
+
 
 class AppInitializer {
   static Future<void> initialize() async {
@@ -13,10 +12,6 @@ class AppInitializer {
       WidgetsFlutterBinding.ensureInitialized();
 
       await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-      final firebaseMessaging = FirebaseMessaging.instance;
-      String? fcmToken = await firebaseMessaging.getToken();
-      log("FCM Token: $fcmToken");
 
       await NotificationService().initNotification();
       await listenToFCM();
--- lib/utilities/shared_preference.dart
+++ lib/utilities/shared_preference.dart
@@ -9,57 +9,102 @@ initializePreferences() async {
 }
 
 class UserPreferences {
+  static set currentServer(String currentServer) {
+    prefs.setString('currentServer', currentServer);
+  }
 
-  static set currentServer(String currentServer) {prefs.setString('currentServer', currentServer);}
   static String get currentServer => prefs.getString('currentServer') ?? "Live";
 
-  static set apiURL(String apiURL) {prefs.setString('apiURL', apiURL);}
+  static set apiURL(String apiURL) {
+    prefs.setString('apiURL', apiURL);
+  }
+
   static String get apiURL => prefs.getString('apiURL') ?? "";
 
-  static set socketURL(String socketURL) {prefs.setString('socketURL', socketURL);}
+  static set socketURL(String socketURL) {
+    prefs.setString('socketURL', socketURL);
+  }
+
   static String get socketURL => prefs.getString('socketURL') ?? "";
 
-  static set webURL(String webURL) {prefs.setString('webURL', webURL);}
+  static set webURL(String webURL) {
+    prefs.setString('webURL', webURL);
+  }
+
   static String get webURL => prefs.getString('webURL') ?? "";
 
-  static set mediaURL(String mediaURL) {prefs.setString('mediaURL', mediaURL);}
+  static set mediaURL(String mediaURL) {
+    prefs.setString('mediaURL', mediaURL);
+  }
+
   static String get mediaURL => prefs.getString('mediaURL') ?? "";
 
-  static set authToken(String authToken) {prefs.setString('authToken', authToken);}
+  static set authToken(String authToken) {
+    prefs.setString('authToken', authToken);
+  }
+
   static String get authToken => prefs.getString('authToken') ?? "";
 
-  static set userId(String id) {prefs.setString('userId', id);}
+  static set userId(String id) {
+    prefs.setString('userId', id);
+  }
+
   static String get userId => prefs.getString('userId') ?? "";
 
-  static set isLogin(bool isLogin) {prefs.setBool('isLogin', isLogin);}
+  static set isLogin(bool isLogin) {
+    prefs.setBool('isLogin', isLogin);
+  }
+
   static bool get isLogin => prefs.getBool('isLogin') ?? false;
 
-  static set loginData(Map<String, dynamic> value) {prefs.setString('loginData', jsonEncode(value));}
-  static Map<String, dynamic> get loginData => jsonDecode(prefs.getString('loginData') ?? "{}");
+  static set loginData(Map<String, dynamic> value) {
+    prefs.setString('loginData', jsonEncode(value));
+  }
 
-  static void bannerShownToday(String date,bool value) {prefs.setBool(date, value);
-}
+  static Map<String, dynamic> get loginData =>
+      jsonDecode(prefs.getString('loginData') ?? "{}");
+
+  static void bannerShownToday(String date, bool value) {
+    prefs.setBool(date, value);
+  }
+
   static bool checkBannerShownToday(date) => prefs.getBool(date) ?? false;
-
 
   // Function Example Syntax To Use
 
-  static set saveMap(Map<String, dynamic> value) {prefs.setString('saveMap', jsonEncode(value));}
-  static Map<String, dynamic> get saveMap => jsonDecode(prefs.getString('saveMap') ?? "{}");
+  static set saveMap(Map<String, dynamic> value) {
+    prefs.setString('saveMap', jsonEncode(value));
+  }
 
-  static set saveList(List<dynamic> value) {prefs.setString('saveList', jsonEncode(value));}
-  static List<dynamic> get saveList => jsonDecode(prefs.getString('saveList') ?? "{}");
+  static Map<String, dynamic> get saveMap =>
+      jsonDecode(prefs.getString('saveMap') ?? "{}");
 
-  static set saveString(String saveString) {prefs.setString('saveString', saveString);}
+  static set saveList(List<dynamic> value) {
+    prefs.setString('saveList', jsonEncode(value));
+  }
+
+  static List<dynamic> get saveList =>
+      jsonDecode(prefs.getString('saveList') ?? "{}");
+
+  static set saveString(String saveString) {
+    prefs.setString('saveString', saveString);
+  }
+
   static String get saveString => prefs.getString('saveString') ?? "";
 
-  static set saveInt(int saveInt) {prefs.setInt('saveInt', saveInt);}
+  static set saveInt(int saveInt) {
+    prefs.setInt('saveInt', saveInt);
+  }
+
   static int get saveInt => prefs.getInt('saveInt') ?? 0;
 
-  static set saveDouble(double saveDouble) {prefs.setDouble('saveDouble', saveDouble);}
+  static set saveDouble(double saveDouble) {
+    prefs.setDouble('saveDouble', saveDouble);
+  }
+
   static double get saveDouble => prefs.getDouble('saveDouble') ?? 0.0;
 
-  static removeUserData () {
+  static removeUserData() {
     prefs.remove('authToken');
     prefs.remove('isLogin');
     prefs.remove('loginData');
